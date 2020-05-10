6eb84630b6be670b097ef70b58de00c70a0f2023
Aman89Django <87711441333@mail.ru>
2020-05-11 00:04:05 +0600

    start react project

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+
+const Header = () => {
+    return     <header className='header'>
+        <img className='logo' src="https://cdn.pixabay.com/photo/2014/04/02/17/01/logo-307673_1280.png" alt="logo"/>
+        header
+    </header>
+}
+
+export default  Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+
+const Profile = () => {
+    return     <div className='content'>
+        <img src="https://www.visitflorida.com/content/dam/visitflorida/en-us/images/11.%20Flagler%20Beachcroppedhoriz.jpg.1280.500.rendition" alt="image"/>
+        Content
+    </div>
+}
+
+export default  Profile;
