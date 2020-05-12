67b226cadcfe6f776b9e895727a143b10ec96257
Aman89Django <87711441333@mail.ru>
2020-05-12 23:00:39 +0600

    project

--- src/App.js
+++ src/App.js
@@ -4,22 +4,23 @@ import Header from "./components/Header";
 import Navbar from "./components/Navbar";
 import Profile from "./components/Profile";
 
-function App() {
-  return (
-      <div className='app-wrapper'>
-          <Header />
+
+const App = () => {
+    return (
+        <div className='app-wrapper'>
+            <Header />
 
 
-          <Navbar />
+            <Navbar />
 
 
 
-          <Profile />
+            <Profile />
 
 
-      </div>
+        </div>
 
-  );
+    );
 }
 
 export default App;
