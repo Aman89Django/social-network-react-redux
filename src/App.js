e9fb2404ea07fdb0d9bc902dcea4c43c6039b75a
Aman89Django <87711441333@mail.ru>
2020-05-21 23:49:06 +0600

    Data change up to index.js with props

--- src/App.js
+++ src/App.js
@@ -6,13 +6,9 @@ import Profile from "./components/Profile/Profile";
 import Dialogs from "./components/Dialogs/Dialogs";
 import {BrowserRouter, Route} from "react-router-dom";
 
-const postsData = [
-    {id: 1, post: "Hi! how are you?", likesCount: 12},
-    {id: 2, post: "It's my first post!", likesCount: 7},
-    {id: 3, post: "It's awesome!", likesCount:25 },
-];
 
-const App = () => {
+
+const App = (props) => {
     return (
         <BrowserRouter>
             <div className='app-wrapper'>
@@ -21,7 +17,7 @@ const App = () => {
                 <Navbar/>
 
                 <div className="app-wrapper-content">
-                    <Route path='/profile' render={ () => <Profile postsData={postsData}/>}/>
+                    <Route path='/profile' render={ () => <Profile postsData={props.postsData}/>}/>
                     <Route path='/dialogs' render={() => <Dialogs/>}/>
                 </div>
 
