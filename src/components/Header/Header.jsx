1e4493e88a54c855448391482c984bb3be9339bb
Aman89Django <87711441333@mail.ru>
2020-08-03 00:17:10 +0600

    cookie, login, auth/me , create: auth-reducer, headerContainer and refactoring in header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,17 @@
 import React from "react";
 import s from "./Header.module.css";
+import {NavLink} from "react-router-dom";
 
 
-const Header = () => {
+const Header = props => {
     return     <header className={s.header}>
         <img src="https://cdn.pixabay.com/photo/2014/04/02/17/01/logo-307673_1280.png" alt="logo"/>
+
+        <div className={s.loginBlock}>
+            {props.isAuth ? props.login
+                : <NavLink to={'/login'}>LOGIN</NavLink>}
+
+        </div>
 
 
     </header>
