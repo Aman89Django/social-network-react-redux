18ce3292886b7142fb1ab4e69491a26d5345a387
Aman89Django <87711441333@mail.ru>
2020-08-27 00:41:51 +0600

    using useEffect

--- src/components/Profile/ProfileInfo/ProfileStatusWithHooks.jsx
+++ src/components/Profile/ProfileInfo/ProfileStatusWithHooks.jsx
@@ -1,39 +1,48 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 
 
-const ProfileStatusWithHooks = (props) =>{
+const ProfileStatusWithHooks = (props) => {
 
     const [editMode, setEditMode] = useState(false);
     const [status, setStatus] = useState(props.status);
 
+    useEffect(() => {
+        setStatus(props.status)
+    }, [props.status]);
 
+
+    // componentDidUpdate(prevProps, prevState) {
+    //     if(prevProps.status !== this.props.status) {
+    //         this.setState({
+    //             status: this.props.status
+    //         })
+    //     }
+    // }
 
 
     const activateEditMode = () => {
         setEditMode(true);
     };
 
-    const deactivateEditMode = () =>{
+    const deactivateEditMode = () => {
         setEditMode(false);
         props.updateStatus(status);
     };
 
-    const onStatusChange = (e) =>{
+    const onStatusChange = (e) => {
         setStatus(e.currentTarget.value)
     }
 
 
-
     return <div>
         Status:
-        { !editMode ?
-        <div><span onDoubleClick={activateEditMode}>{status || 'NO status'}</span></div>
-        : <div><input onChange={onStatusChange} onBlur={deactivateEditMode} autoFocus={true} value={status}/></div>}
+        {!editMode ?
+            <div><span onDoubleClick={activateEditMode}>{status || 'NO status'}</span></div>
+            : <div><input onChange={onStatusChange} onBlur={deactivateEditMode} autoFocus={true} value={status}/></div>}
     </div>
 
 
 };
-
 
 
 export default ProfileStatusWithHooks;
