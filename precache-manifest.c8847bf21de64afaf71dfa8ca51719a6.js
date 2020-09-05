86323e78acc122e85db851e76a0e1d859d054812
Aman89Django <87711441333@mail.ru>
2020-09-05 21:43:21 +0600

    Updates

---
+++ precache-manifest.c8847bf21de64afaf71dfa8ca51719a6.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a78a229694a2d00141f116c008d77250",
+    "url": "/react-lessons/index.html"
+  },
+  {
+    "revision": "f9bf71efc79fbdbc6492",
+    "url": "/react-lessons/static/css/3.3206a1c2.chunk.css"
+  },
+  {
+    "revision": "99b4a183ac6b1dcbada4",
+    "url": "/react-lessons/static/css/4.bc33074d.chunk.css"
+  },
+  {
+    "revision": "b46f2324d4cbee72129d",
+    "url": "/react-lessons/static/css/main.fbc32c77.chunk.css"
+  },
+  {
+    "revision": "5088987cfa4182b8c062",
+    "url": "/react-lessons/static/js/2.b0243644.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/react-lessons/static/js/2.b0243644.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9bf71efc79fbdbc6492",
+    "url": "/react-lessons/static/js/3.08317330.chunk.js"
+  },
+  {
+    "revision": "99b4a183ac6b1dcbada4",
+    "url": "/react-lessons/static/js/4.49f5f7ac.chunk.js"
+  },
+  {
+    "revision": "b46f2324d4cbee72129d",
+    "url": "/react-lessons/static/js/main.31831061.chunk.js"
+  },
+  {
+    "revision": "85b8a06a6aa1be98f313",
+    "url": "/react-lessons/static/js/runtime-main.0d311f43.js"
+  },
+  {
+    "revision": "94c3a24b00947c9d2a992b4ca195df38",
+    "url": "/react-lessons/static/media/avatar_user.94c3a24b.png"
+  },
+  {
+    "revision": "0d0345f662a757ef17eef497fa8e83fc",
+    "url": "/react-lessons/static/media/preloader.0d0345f6.gif"
+  }
+]);
