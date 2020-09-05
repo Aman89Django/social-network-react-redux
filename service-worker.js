c8b69a9e84c7873d2359e819781768a1984635cb
Aman89Django <87711441333@mail.ru>
2020-09-05 22:12:57 +0600

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-lessons/precache-manifest.edcce99e69f89e818c958e42b063a687.js"
+  "/react-lessons/precache-manifest.74e3a0404c7c21be474ef82a8d459d3c.js"
 );
 
 self.addEventListener('message', (event) => {
